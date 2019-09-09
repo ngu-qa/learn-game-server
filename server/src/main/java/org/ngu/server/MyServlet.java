456a7a6446041f41c1e31c9103d4d64140123059
liang.chen <liang.chen@happyelements.com>
2019-09-09 16:37:29 +0800

    initial commit

---
+++ server/src/main/java/org/ngu/server/MyServlet.java
@@ -0,0 +1,24 @@
+package org.ngu.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyServlet", urlPatterns = "/MyServlet")
+public class MyServlet extends HttpServlet {
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("MyServlet configuration with annotation");
+    }
+
+}
